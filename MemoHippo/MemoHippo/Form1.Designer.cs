ad3c36fe33d1963c8b523778ee70379620cc7390
real <real@163.com>
2023-10-27 09:56:03 +0800

    init commit

---
+++ MemoHippo/MemoHippo/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿
+namespace MemoHippo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.flowLayoutPanel3 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.flowLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AllowDrop = true;
+            this.flowLayoutPanel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(57, 26);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(254, 686);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.AllowDrop = true;
+            this.flowLayoutPanel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.flowLayoutPanel2.Controls.Add(this.label1);
+            this.flowLayoutPanel2.Controls.Add(this.label2);
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(414, 26);
+            this.flowLayoutPanel2.Margin = new System.Windows.Forms.Padding(5);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Padding = new System.Windows.Forms.Padding(5);
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(272, 686);
+            this.flowLayoutPanel2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AllowDrop = true;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Font = new System.Drawing.Font("宋体", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.ForeColor = System.Drawing.Color.DarkGray;
+            this.label1.Location = new System.Drawing.Point(10, 10);
+            this.label1.Margin = new System.Windows.Forms.Padding(5);
+            this.label1.Name = "label1";
+            this.label1.Padding = new System.Windows.Forms.Padding(3);
+            this.label1.Size = new System.Drawing.Size(230, 53);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // flowLayoutPanel3
+            // 
+            this.flowLayoutPanel3.AllowDrop = true;
+            this.flowLayoutPanel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.flowLayoutPanel3.Location = new System.Drawing.Point(813, 27);
+            this.flowLayoutPanel3.Name = "flowLayoutPanel3";
+            this.flowLayoutPanel3.Size = new System.Drawing.Size(254, 686);
+            this.flowLayoutPanel3.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AllowDrop = true;
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Font = new System.Drawing.Font("宋体", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.ForeColor = System.Drawing.Color.DarkGray;
+            this.label2.Location = new System.Drawing.Point(10, 73);
+            this.label2.Margin = new System.Windows.Forms.Padding(5);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(230, 53);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1204, 739);
+            this.Controls.Add(this.flowLayoutPanel3);
+            this.Controls.Add(this.flowLayoutPanel2);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.ForeColor = System.Drawing.Color.White;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.flowLayoutPanel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel3;
+    }
+}
+
---
+++ MemoHippo/MemoHippo/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System.Linq;
+using System.Windows.Forms;
+
+namespace MemoHippo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            flowLayoutPanel1.DragEnter += flowLayoutPanel1_DragEnter;
+            flowLayoutPanel1.DragDrop += flowLayoutPanel1_DragDrop;
+            flowLayoutPanel2.DragEnter += flowLayoutPanel1_DragEnter;
+            flowLayoutPanel2.DragDrop += flowLayoutPanel1_DragDrop;
+            flowLayoutPanel3.DragEnter += flowLayoutPanel1_DragEnter;
+            flowLayoutPanel3.DragDrop += flowLayoutPanel1_DragDrop;
+            label1.MouseDown += label_MouseDown;
+            label2.MouseDown += label_MouseDown;
+        }
+
+        private void flowLayoutPanel1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.Text))
+            {
+                e.Effect = DragDropEffects.Move;
+            }
+        }
+
+        private void flowLayoutPanel1_DragDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.Text))
+            {
+                string[] names = ((string)e.Data.GetData(DataFormats.StringFormat)).Split('.');
+
+                // 根据 Name 查找 Label 控件
+                var label = Controls[names[0]].Controls[names[1]];
+
+                if (label != null)
+                {
+
+                    // 从原始 FlowLayoutPanel 中移除 Label
+                    label.Parent.Controls.Remove(label);
+
+                    // 添加 Label 到目标 FlowLayoutPanel
+                    (sender as Control).Controls.Add(label);
+                }
+            }
+        }
+
+        private void label_MouseDown(object sender, MouseEventArgs e)
+        {
+            Label label = (Label)sender;
+            label.DoDragDrop(label.Parent.Name + "." + label.Name, DragDropEffects.Move);
+        }
+    }
+}
