063065daf9b725ebe07d54ac696b18e61324a2f9
narlon <narlonzjh@163.com>
2024-08-06 09:12:22 +0800

    add memo attr item

---
+++ MemoHippo/MemoHippo/UIS/Panels/UCBGPropertyAttrItem.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace MemoHippo.UIS.Panels
+{
+    class UCBGPropertyAttrItem : Label
+    {
+        private bool checked1;
+        //private Image icon;
+        private Color foreColor;
+        private Color bgColor = Color.White;
+        public bool Checked { get { return checked1; } set { checked1 = value; Invalidate(); } }
+
+        public override string Text { get { return base.Text; } set { base.Text = value; CheckWords(value); Invalidate(); } }
+
+        public UCBGPropertyAttrItem()
+        {
+            AutoSize = true;
+            Click += UCBGPropertyAttrItem_Click;
+        }
+
+        private void CheckWords(string wrd)
+        {
+            // 定义颜色映射  
+            Dictionary<string, Tuple<Color, Color>> colorMap = new Dictionary<string, Tuple<Color, Color>>
+            {
+                { "收纳", new Tuple<Color, Color>(Color.DimGray, Color.BurlyWood) },
+                { "牌套", new Tuple<Color, Color>(Color.DimGray, Color.LightGray) },
+                { "3d", new Tuple<Color, Color>(Color.Firebrick, Color.White) },
+                { "众筹", new Tuple<Color, Color>(Color.Cyan, Color.White) },
+            };
+
+            // 默认颜色  
+            foreColor = Color.LawnGreen;
+            bgColor = Color.White; // 假设默认背景色是白色  
+
+            foreach(var map in colorMap)
+            {
+                if(wrd.Contains(map.Key))
+                {
+                    foreColor = map.Value.Item1;
+                    bgColor = map.Value.Item2;
+                    break;
+                }
+            }
+            // 检查是否有匹配的颜色设置  
+            //if (colorMap.TryGetValue(wrd, out var colors))
+            //{
+            //    foreColor = colors.Item1;
+            //    bgColor = colors.Item2;
+            //}
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            if (checked1)
+            {
+                if (bgColor != Color.White)
+                    using (var b = new SolidBrush(bgColor))
+                        e.Graphics.FillRectangle(b, 0, 0, Width, Height);
+                using (var b = new SolidBrush(foreColor))
+                    e.Graphics.DrawString(Text, Font, b, 0, 0);
+            }
+            else
+            {
+                e.Graphics.DrawString(Text, Font, Brushes.DimGray, 0, 0);
+            }
+        }
+
+        private void UCBGPropertyAttrItem_Click(object sender, EventArgs e)
+        {
+            checked1 = !checked1;
+            Invalidate();
+        }
+
+    }
+}
